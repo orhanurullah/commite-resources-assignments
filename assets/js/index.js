9437699db43f1b69e3d7ebadb2a0886fb18b2c04
orhanurullah <orhann.1512@gmail.com>
2022-03-20 01:17:28 +0300

    Create Js File, Css Files and Create Responsive Pages

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+/**
+ * Variables html elements
+ */
+const closeButon = document.querySelector('#closebuton');
+const openButon = document.querySelector('#openbuton');
+
+/**
+ * Close button event listener
+ * change style with and z-index value
+ */
+closeButon.addEventListener('click', () => {
+    const sidebar = document.querySelector('#sidebar-nav');
+    sidebar.style.width = 0;
+    sidebar.style.zIndex = 0;
+});
+
+/**
+ * Open button event listener
+ * 
+ */
+
+openButon.addEventListener('click', () => {
+    const sidebar = document.querySelector('#sidebar-nav');
+    sidebar.style.width = "15%";
+    sidebar.style.zIndex = 1;
+});
